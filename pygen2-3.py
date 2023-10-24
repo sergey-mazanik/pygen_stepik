c563427c136121ef3a1b9173e4118f0ec679bd71
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-10-24 14:48:23 +0300

    add 2 and 3 units

---
+++ pygen2-3.py
@@ -0,0 +1,139 @@
+# print(input(), input(), input(), sep='\n')
+
+# a, b, c = input(), input(), input()
+# print(c, b, a, sep='\n')
+
+# a, b, c, d = input(), input(), input(), input()
+# print(b, c, d, sep=a)
+
+# print('Привет, ', input(), sep='', end='!')
+
+# num = int(input())
+# print(num,num+1,num+2,sep='\n')
+
+# count = 0
+# for i in range(3):
+#     a = int(input())
+#     count += a
+# print(count)
+
+# rebro = int(input())
+# print(f'''Объем = {rebro ** 3}
+# Площадь полной поверхности = {6 * rebro ** 2}''')
+
+# a, b = int(input()), int(input())
+# print(f'{3 * (a + b) ** 3 + 275 * b ** 2 - 127 * a - 41}')
+
+# num = int(input())
+# print(f'''Следующее за числом {num} число: {num + 1}
+# Для числа {num} предыдущее число: {num - 1}''')
+
+# mon = int(input())
+# sb = int(input())
+# clav = int(input())
+# mouse = int(input())
+# print((mon + sb + clav + mouse) * 3)
+
+# a, b = int(input()), int(input())
+# print(f'''{a} + {b} = {a + b}
+# {a} - {b} = {a - b}
+# {a} * {b} = {a * b}''')
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# print(f'{a1 + d * (n - 1)}')
+
+# num = int(input())
+# print(num, num*2, num*3, num*4, num*5, sep='---')
+
+# b1 = int(input())
+# q = int(input())
+# n = int(input())
+# print(f'{b1 * q ** (n - 1)}')
+
+# print(int(input()) // 100)
+
+# a = int(input())
+# b = int(input())
+# print(b // a, b % a, sep='\n')
+
+# num = int(input())
+# print(num // 2 + num % 2)
+
+# num = int(input())
+# print((num + 3) // 4)
+
+# minutes = int(input())
+# print(f'{minutes} мин - это {minutes // 60} час {minutes % 60} минут.')
+
+# num = int(input())
+# last_digit = num % 10
+# first_digit = num // 10
+# print('Число десятков =', first_digit)
+# print('Число единиц =', last_digit)
+
+# num = int(input())
+# last_digit = num % 10
+# first_digit = num // 10
+# print('Сумма цифр =', last_digit + first_digit)
+
+# num = int(input())
+# last_digit = num % 10
+# first_digit = num // 10
+# print('Искомое число =', last_digit * 10 + first_digit)
+
+# num = int(input())
+# digit3 = num % 10
+# digit2 = (num // 10) % 10
+# digit1 = num // 100
+# print(digit1, digit2, digit3, sep=',')
+
+# num = int(input())
+# first = num % 10
+# second = (num // 10) % 10
+# third = num // 100
+# print(f'''Сумма цифр = {first + second + third}
+# Произведение цифр = {first * second * third}''')
+
+# num = int(input())
+# c = num % 10
+# b = (num // 10) % 10
+# a = num // 100
+# print(f'''{a}{b}{c}
+# {a}{c}{b}
+# {b}{a}{c}
+# {b}{c}{a}
+# {c}{a}{b}
+# {c}{b}{a}''')
+
+# num = int(input())
+# first = num % 10
+# second = (num // 10) % 10
+# third = (num // 100) % 10
+# fourth = num // 1000
+# print(f'''Цифра в позиции тысяч равна {fourth}
+# Цифра в позиции сотен равна {third}
+# Цифра в позиции десятков равна {second}
+# Цифра в позиции единиц равна {first}''')
+
+# num = 12345
+# print(num % 10000 // 1000)
+
+# print(f'''*****************
+# *               *
+# *               *
+# *****************''')
+
+# a = int(input())
+# b = int(input())
+# print(f'''Квадрат суммы {a} и {b} равен {(a + b) ** 2}
+# Сумма квадратов {a} и {b} равна {a ** 2 + b ** 2}''')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print(a ** b + c ** d)
+
+# print(int(input())*123)
