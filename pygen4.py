aa1dca216aa3af8027fd82433c047903c4bc9aca
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-10-24 17:34:53 +0300

    add 4.1 unit

---
+++ pygen4.py
@@ -0,0 +1,51 @@
+# a, b = input(), input()
+# print('Пароль принят' if a == b else 'Пароль не принят')
+
+# print('Четное' if int(input()) % 2 == 0 else 'Нечетное')
+
+# num = int(input())
+# fourth = num % 10
+# third = (num // 10) % 10
+# second = (num // 100) % 10
+# first = num // 1000
+# print('ДА' if (first + fourth) == (second - third) else 'НЕТ')
+
+# print('Доступ запрещен' if int(input()) < 18 else 'Доступ разрешен')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print('YES' if (a < b < c and c - b == b - a) or (a > b > c and a - b == b - c) else 'NO')
+
+# a = int(input())
+# b = int(input())
+# print(b if a > b else a)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# lst = [a, b, c, d]
+# print(min(lst))
+
+# age = int(input())
+# if 0 < age <= 13:
+#     print('детство')
+# elif 14 <= age < 25:
+#     print('молодость')
+# elif 25 <= age < 60:
+#     print('зрелость')
+# elif age >= 60:
+#     print('старость')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# lst = []
+# if a > 0:
+#     lst.append(a)
+# if b > 0:
+#     lst.append(b)
+# if c > 0:
+#     lst.append(c)
+# print(sum(lst))
