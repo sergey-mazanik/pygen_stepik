a7e8733c8de07b2992420f7db549e7bec719a2dc
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-10-25 18:06:11 +0300

    add some tasks for 4.2 unit

--- pygen4.py
+++ pygen4.py
@@ -49,3 +49,25 @@
 # if c > 0:
 #     lst.append(c)
 # print(sum(lst))
+
+# num = int(input())
+# print('Принадлежит' if -1 < num < 17 else 'Не принадлежит')
+
+# # num = int(input())
+# print('Не принадлежит' if -3 < int(input()) < 7 else 'Принадлежит')
+
+# num = int(input())
+# print('Принадлежит' if -30 < num <= -2 or 7 < num <= 25 else 'Не принадлежит')
+
+# num = input()
+# print('YES' if len(num) == 4 and (int(num) % 7 == 0 or int(num) % 17 == 0) else 'NO')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print('YES' if (a + b > c) and (a + c > b) and (b + c > a) else 'NO')
+
+# year = int(input())
+# print('YES' if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 'NO')
+
+
