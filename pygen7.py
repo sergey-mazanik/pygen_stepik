22d8cad9e841d22bb6522ce0f28337e33c42e83d
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-11-01 13:18:51 +0300

    add 7.1 and 7.2 units

---
+++ pygen7.py
@@ -0,0 +1,73 @@
+####### LOOPS #######
+
+# for i in range(10):
+#     print('Python is awesome!')
+
+# phrase = input()
+# num = int(input())
+# for i in range(num):
+#     print(phrase)
+
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# print('E')
+# for i in range(9):
+#     print('TTTTT')
+# print('G')
+
+# num = int(input())
+# for i in range(num):
+#     print('*' * 19)
+
+# name = input()
+# for i in range(10):
+#     print(i, name)
+
+# from math import pow
+# num = int(input())
+# for i in range(num + 1):
+#     print(f'Квадрат числа {i} равен {int(pow(i, 2))}')
+
+# num = int(input())
+# for i in range(num):
+#     print('*' * (num - i))
+
+# m = int(input())
+# p = int(input())
+# n = int(input())
+# koef = (1 + p / 100)
+# for i in range(n):
+#     print(i + 1, m * (koef ** i))
+
+# m = int(input())
+# n = int(input())
+# for i in range(m, n+1):
+#     print(i)
+
+# m = int(input())
+# n = int(input())
+# if n > m:
+#     for i in range(m, n+1):
+#         print(i)
+# elif n < m:
+#     for i in range(m, n-1, -1):
+#         print(i)
+# else:
+#     print(m)
+
+# m = int(input())
+# n = int(input())
+# for i in range((m + m % 2 - 1), n-1, -2):
+#     print(i)
+
+# m = int(input())
+# n = int(input())
+# for i in range(m, n + 1):
+#     if i % 17 == 0 or i % 10 == 9 or (i % 3 == 0 and i % 5 == 0):
+#         print(i)
+
+# num = int(input())
+# for i in range(1, 11):
+#     print(f'{num} x {i} = {num * i}')
