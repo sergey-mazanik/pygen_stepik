c93ab89407c4bb9ced667c2686181e1f786d7613
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-11-02 13:32:22 +0300

    add part of 7.3 unit

--- pygen7.py
+++ pygen7.py
@@ -71,3 +71,67 @@
 # num = int(input())
 # for i in range(1, 11):
 #     print(f'{num} x {i} = {num * i}')
+
+# from math import pow
+#
+# num1 = int(input())
+# num2 = int(input())
+# count = 0
+# for i in range(num1, num2 + 1):
+#     if pow(i, 3) % 10 == 4 or pow(i, 3) % 10 == 9:
+#         count += 1
+# print(count)
+
+# num = int(input())
+# summa = 0
+# for _ in range(1, num + 1):
+#     a = int(input())
+#     summa += a
+# print(summa)
+
+# from math import log, e
+#
+# n = int(input())
+# q = 0
+# for i in range(1, n + 1):
+#     a = 1 / i
+#     q += a
+# print(q - log(n, e))
+
+# from math import pow
+#
+# num = int(input())
+# count = 0
+# for i in range(1, num + 1):
+#     if pow(i, 2) % 10 == 2 or pow(i, 2) % 10 == 5 or pow(i, 2) % 10 == 8:
+#         count += i
+# print(count)
+
+# from math import factorial
+#
+# print(factorial(int(input())))
+
+# summa = 1
+# for _ in range(10):
+#     num = int(input())
+#     if num != 0:
+#         summa *= num
+# print(summa)
+
+# num = int(input())
+# count = 0
+# for i in range(1, num + 1):
+#     if num % i == 0:
+#         count += i
+# print(count)
+
+# num = int(input())
+# count = 0
+# for i in range(num):
+#     if i % 2 == 0:
+#         count += i + 1
+#     elif i % 2 != 0:
+#         count -= i + 1
+# print(count)
+
+
