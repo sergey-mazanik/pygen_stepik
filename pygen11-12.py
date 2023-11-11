b3fe29c6b2cdb50446ba6d75a5f56a2fcfc353cf
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-11-11 19:56:33 +0300

    add 11.1 unit

---
+++ pygen11-12.py
@@ -0,0 +1,8 @@
+######## LIST ########
+
+# print(list(range(1, int(input()) + 1)))
+
+# abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+#        'w', 'x', 'y', 'z']
+# n = int(input())
+# print(abc[0:n])
