6eaec3bdedb785d71c6f96b87aa266e34d0eabb8
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-11-16 00:40:56 +0300

    add part of 11.7 unit

--- pygen11-12.py
+++ pygen11-12.py
@@ -234,3 +234,15 @@
 # print(*lst)
 # lst.sort(reverse=True)
 # print(*lst)
+
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#             'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+#             'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+# new_keywords = [c[1:] for c in keywords]
+# print(new_keywords)
+
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#             'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+#             'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+# lengths = [len(keyword) for keyword in keywords]
+# print(lengths)
