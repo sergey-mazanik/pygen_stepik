1a8be576e0a6ade695d6b436b4d8dd904f8f2846
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2023-12-26 22:17:12 +0300

    add units 13 and 14

--- pygen13-14.py
+++ pygen13-14.py
@@ -269,3 +269,164 @@
 #
 # psw = input()
 # print(is_valid_password(psw))
+
+
+# def is_correct_bracket(text):
+#     count = 0
+#     for i in range(len(text)):
+#         if count < 0:
+#             break
+#         elif text[0] == ')':
+#             count = -1
+#         elif text[i] == '(':
+#             count += 1
+#         else:
+#             count -= 1
+#     if count == 0:
+#         return True
+#     else:
+#         return False
+#
+#
+# txt = input()
+# print(is_correct_bracket(txt))
+
+
+# def convert_to_python_case(text):
+#     st = text[0].lower()
+#     for i in text[1:]:
+#         if i.isupper():
+#             st = st + '_' + i.lower()
+#         else:
+#             st += i.lower()
+#     return st
+#
+#
+# txt = input()
+# print(convert_to_python_case(txt))
+
+
+# def get_middle_point(x1, y1, x2, y2):
+#     x = (x1 + x2) / 2
+#     y = (y1 + y2) / 2
+#     return x, y
+#
+#
+# x_1, y_1 = int(input()), int(input())
+# x_2, y_2 = int(input()), int(input())
+# x, y = get_middle_point(x_1, y_1, x_2, y_2)
+# print(x, y)
+
+
+# from math import pi
+#
+#
+# def get_circle(radius):
+#     length = 2 * pi * radius
+#     square = pi * radius ** 2
+#     return length, square
+#
+#
+# r = float(input())
+# length, square = get_circle(r)
+# print(length, square)
+
+
+# def solve(a, b, c):
+#     d = b ** 2 - 4 * a * c
+#     x1 = (-b + d ** 0.5) / (2 * a)
+#     x2 = (-b - d ** 0.5) / (2 * a)
+#     x3 = (-b) / (2 * a)
+#     if d > 0:
+#         return min(x1, x2), max(x1, x2)
+#     elif d == 0:
+#         return x3, x3
+#     else:
+#         return 'Нет корней'
+#
+#
+# a, b, c = int(input()), int(input()), int(input())
+# x1, x2 = solve(a, b, c)
+# print(x1, x2)
+
+
+# def get_shipping_cost(quantity):
+#     if quantity == 1:
+#         return 1000
+#     else:
+#         return 1000 + 120 * (quantity - 1)
+#
+#
+# n = int(input())
+# print(get_shipping_cost(n))
+
+
+# from math import factorial
+# def compute_binom(n, k):
+#     bin = factorial(n) / (factorial(k) * factorial(n - k))
+#     return int(bin)
+#
+#
+# n = int(input())
+# k = int(input())
+# print(compute_binom(n, k))
+
+
+# def number_to_words(num):
+#     dig = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+#     dec = ['', 'десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят',
+#            'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
+#     teen = ['', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать',
+#             'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+#     if 0 < num <= 9:
+#         return dig[num]
+#     if num % 10 == 0:
+#         return dec[int(num / 10)]
+#     if 10 < num <= 19:
+#         return teen[num - 10]
+#     if 19 < num < 100 and num % 10 != 0:
+#         return dec[num // 10] + ' ' + dig[num % 10]
+#
+#
+# n = int(input())
+# print(number_to_words(n))
+
+
+# def get_month(language, number):
+#     lng_ru = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь',
+#               'декабрь']
+#
+#     lng_en = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+#               'november', 'december']
+#     if language == 'ru':
+#         return lng_ru[number-1]
+#     if language == 'en':
+#         return lng_en[number-1]
+#
+# lan = input()
+# num = int(input())
+# print(get_month(lan, num))
+
+
+# def is_magic(date):
+#     lst = date.split('.')
+#     if int(lst[0]) * int(lst[1]) == int(lst[2][2:]):
+#         return True
+#     else:
+#         return False
+#
+#
+# date = input()
+# print(is_magic(date))
+
+
+def is_pangram(text):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    for i in abc:
+        if i not in text.lower():
+            return False
+    return True
+
+
+text = input()
+print(is_pangram(text))
