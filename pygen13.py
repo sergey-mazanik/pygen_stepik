1d9bd1abe28f22fa0cdea2d90da65797fd5680b2
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2023-12-26 18:25:02 +0300

    add part of unit 13

--- pygen13.py
+++ pygen13.py
@@ -145,9 +145,127 @@
 # print(is_valid_triangle(a, b, c))
 
 
+# def is_prime(num):
+#     lst = []
+#     for i in range(1, num + 1):
+#         if num % i == 0:
+#             lst.append(i)
+#     if len(lst) == 2:
+#         return True
+#     else:
+#         return False
+#
+#
+# n = int(input())
+# print(is_prime(n))
+
+
+# объявление функции
+# def get_next_prime(num):
+#     count = num
+#     while True:
+#         count += 1
+#         if is_prime(count):
+#             return count
+#             break
+#
+#
+# n = int(input())
+# print(get_next_prime(n))
+
+
+# объявление функции
+# def is_password_good(password):
+#     count_up = 0
+#     count_low = 0
+#     count_dig = 0
+#     if len(password) < 8:
+#         return False
+#     for c in password:
+#         if c.isupper():
+#             count_up += 1
+#         if c.islower():
+#             count_low += 1
+#         if c.isdigit():
+#             count_dig += 1
+#     if count_up != 0 and count_low != 0 and count_dig != 0:
+#         return True
+#     else:
+#         return False
+#
+#
+# txt = input()
+# print(is_password_good(txt))
+
+
+# def is_one_away(word1, word2):
+#     count = 0
+#     if len(word1) != len(word2):
+#         return False
+#     for i in range(len(word1)):
+#         if word1[i] != word2[i]:
+#             count += 1
+#     if count == 1:
+#         return True
+#     else:
+#         return False
+#     # else:
+#     #     return False
+#
+#
+#
+# txt1 = input()
+# txt2 = input()
+# print(is_one_away(txt1, txt2))
+
+
+# def is_palindrome(text):
+#     text1 = "".join(i for i in text if i.isalpha())
+#     if text1.lower()[:] == text1.lower()[::-1]:
+#         return True
+#     else:
+#         return False
+#
+#
+# txt = input()
+# print(is_palindrome(txt))
+
 def is_prime(num):
-    pass
+    lst = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            lst.append(i)
+    if len(lst) == 2:
+        return True
+    else:
+        return False
 
+def is_palindrome(text):
+    text1 = "".join(i for i in text if i.isalpha())
+    if text1.lower()[:] == text1.lower()[::-1]:
+        return True
+    else:
+        return False
 
-n = int(input())
-print(is_prime(n))
+def is_valid_password(password):
+    password = password.split(':')
+    count = 0
+    a = password[0]
+    b = int(password[1])
+    c = int(password[2])
+    if len(password) != 3:
+        return False
+    if a[:] == a[::-1]:
+        count += 1
+    if is_prime(b):
+        count += 1
+    if c % 2 == 0:
+        count += 1
+
+    if count == 3:
+        return True
+    else:
+        return False
+
+psw = input()
+print(is_valid_password(psw))
